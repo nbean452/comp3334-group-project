b30bd4da2830b0aeaf072c113d57d00d1436c197
nicb112 <91611250+nicb112@users.noreply.github.com>
2022-04-07 16:23:44 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,133 @@
+from flask import Flask, render_template, url_for, redirect, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+from datetime import datetime
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'comp3334-group-project'
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    # taskid = db.relationship('Task', backref='user')
+    # TODO
+
+# TODO
+# class Task(db.Model, UserMixin):
+#     id = db.Column(db.Integer, primary_key=True)
+#     ownerid = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     name = db.Column(db.String(20), nullable=False)
+#     completion = db.Column(db.Boolean)
+#     desc = db.Column(db.String(400), nullable=True)
+#     creationdate = db.Column(db.DateTime, nullable=False,
+#                              default=datetime.now)
+#     # id, ownerid, name, completion, description, creation date
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                "That username already exists. Please choose a different one."
+            )
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Login")
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    msg = ""
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('main'))
+            else:
+                msg = "Username / Password is incorrect."
+                return render_template('login.html', form=form, msg=msg)
+        else:
+            msg = "Username / Password is incorrect."
+            return render_template('login.html', form=form, msg=msg)
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect((url_for('login')))
+
+    return render_template('register.html', form=form)
+
+
+@app.route('/main')
+@login_required
+def main():
+
+    user = User.query.filter_by(username=current_user.username).first()
+    task_list = Task.query.filter_by(ownerid=user.id)
+
+    return render_template('main.html', user=user, task_list=task_list)
+
+
+if __name__ == '__main__':
+
+    db.create_all()
+
+    app.run(debug=True)
