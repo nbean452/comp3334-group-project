0570b3b3f0a7758ede2508508660599e5f456d85
nicb112 <91611250+nicb112@users.noreply.github.com>
2022-04-11 12:34:56 +0800

    can upload art

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from email.mime import base
+from enum import unique
 from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
@@ -6,6 +8,10 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 from datetime import datetime
+from flask import Flask, request, Response
+from werkzeug.utils import secure_filename
+from sqlalchemy.orm import relationship
+import base64
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -29,10 +35,22 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    arts = relationship("Art", backref="user")
     # taskid = db.relationship('Task', backref='user')
     # TODO
 
 # TODO
+
+
+class Art(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+    img = db.Column(db.Text, nullable=False, unique=True)
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    owner_id = db.Column(db.Integer)
+    creationdate = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    mimetype = db.Column(db.Text, nullable=False)
+
 # class Task(db.Model, UserMixin):
 #     id = db.Column(db.Integer, primary_key=True)
 #     ownerid = db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -70,7 +88,19 @@ class LoginForm(FlaskForm):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    images = Art.query.all()
+
+    if not images:
+        return render_template('index.html')
+
+    return_images = []
+
+    for image in images:
+        base64_image = base64.b64encode(image.img)
+        return_images.append(base64_image.decode("UTF-8"))
+
+    return render_template('index.html', images=return_images)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -121,9 +151,37 @@ def register():
 def main():
 
     user = User.query.filter_by(username=current_user.username).first()
-    task_list = Task.query.filter_by(ownerid=user.id)
+    # task_list = Task.query.filter_by(ownerid=user.id)
 
-    return render_template('main.html', user=user, task_list=task_list)
+    images = Art.query.filter_by(owner_id=current_user.id)
+    return_images = []
+
+    for image in images:
+        base64_image = base64.b64encode(image.img)
+        return_images.append(base64_image.decode("UTF-8"))
+
+    return render_template('main.html', user=user, images=return_images)
+
+
+@app.route('/main/upload', methods=['POST'])
+@login_required
+def upload():
+    picname = request.form.get('picname')
+    pic = request.files['pic']
+    if not pic:
+        return 'Not Picture!', 400
+
+    filename = secure_filename(pic.filename)
+    mimetype = pic.mimetype
+    if not filename or not mimetype:
+        return 'Bad upload!', 400
+
+    art = Art(img=pic.read(), name=picname, mimetype=mimetype,
+              creator_id=current_user.id, owner_id=current_user.id)
+    db.session.add(art)
+    db.session.commit()
+
+    return redirect((url_for('main')))
 
 
 if __name__ == '__main__':
