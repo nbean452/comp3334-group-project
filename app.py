61035426cfe9e819ebf9221d61477d773319b2bf
nicb112 <91611250+nicb112@users.noreply.github.com>
2022-04-13 21:20:54 +0800

    Only thing left to do is frontend-related stuff
    Co-authored-by: mikecoa <mikecoa@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-import os
 from threading import Thread
-from email.mime import base
-from enum import unique
 from flask import Flask, render_template, url_for, redirect, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
@@ -10,13 +7,11 @@ from itsdangerous import Serializer
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
-#from flaskblog import db, login_manager, app, mail
-#from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from datetime import datetime
-from flask import Flask, request
 from werkzeug.utils import secure_filename
 import base64
 from flask_mail import Message, Mail
+import re
 
 
 app = Flask(__name__)
@@ -74,12 +69,8 @@ class User(db.Model, UserMixin):
     created_arts = db.relationship(
         'Art', secondary=users_created_arts, backref='creator')
 
-    # owned_arts = db.relationship("Art", backref="owner")
-    # created_arts = db.relationship("Art", backref="creator")
 
     def get_reset_token(self, expires_sec=1800):
-        # s = Serializer(app.config['SECRET_KEY'], expires_sec)
-        # return s.dumps({'user_id': self.id}).decode('utf-8')
         s = Serializer(app.config['SECRET_KEY'])
         return s.dumps({'user_id': self.id})
 
@@ -101,14 +92,7 @@ class Art(db.Model):
                              default=datetime.now)
     mimetype = db.Column(db.Text, nullable=False)
     price = db.Column(db.Integer, default=0)
-
     transactions = db.relationship("Transaction", backref='art')
-
-    # transc = db.relationship(
-    #     'Art', secondary=transactions, backref='transaction')
-
-    # creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    # owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class Transaction(db.Model):
@@ -116,25 +100,29 @@ class Transaction(db.Model):
     date = db.Column(db.DateTime, nullable=False,
                      default=datetime.now)
     art_id = db.Column(db.Integer, db.ForeignKey('art.id'))
-
     seller_id = db.Column(db.Integer)
     buyer_id = db.Column(db.Integer)
-
     price = db.Column(db.Integer, default=0, nullable=False)
 
-    # users = db.relationship("User", backref='transactions', lazy='dynamic')
 
 
 class RegisterForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(
         min=4, max=20)], render_kw={"placeholder": "Username"})
     email = StringField(validators=[InputRequired(), Length(
-        min=4, max=1000)], render_kw={"placeholder": "Email"})
+        min=4, max=200)], render_kw={"placeholder": "Email"})
     password = PasswordField(validators=[InputRequired(), Length(
-        min=4, max=20)], render_kw={"placeholder": "Password"})
+        min=8, max=20)], render_kw={"placeholder": "Password"}, )
+    password_confirm = PasswordField(validators=[InputRequired(), Length(
+        min=8, max=20)], render_kw={"placeholder": "Enter password again"})
+    confirm=PasswordField()
     recaptcha = RecaptchaField()
     submit = SubmitField("Register")
-
+    def check_password(self, password, password_confirm):
+        if password.data!=password_confirm.data:
+            raise ValidationError(
+                "Password is not the same! Please type again."
+            )
     def validate_username(self, username):
         existing_user_username = User.query.filter_by(
             username=username.data).first()
@@ -142,13 +130,11 @@ class RegisterForm(FlaskForm):
             raise ValidationError(
                 "That username already exists. Please choose a different one."
             )
-
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError(
                 "That email is taken. Please choose a different one.")
-
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(
@@ -158,12 +144,16 @@ class LoginForm(FlaskForm):
     recaptcha = RecaptchaField()
     submit = SubmitField("Login")
 
+class AuthenticateForm(FlaskForm):
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Buy")    
+
 
 class RequestResetForm(FlaskForm):
     email = StringField(validators=[InputRequired(), Length(
         min=4, max=1000)], render_kw={"placeholder": "Email"})
     submit = SubmitField("Request Password Reset")
-
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is None:
@@ -173,38 +163,33 @@ class RequestResetForm(FlaskForm):
 
 class ResetPasswordForm(FlaskForm):
     password = PasswordField(validators=[InputRequired(), Length(
-        min=4, max=20)], render_kw={"placeholder": "New password"})
+        min=8, max=20)], render_kw={"placeholder": "New password"})
+    password_confirm = PasswordField(validators=[InputRequired(), Length(
+        min=8, max=20)], render_kw={"placeholder": "Enter password again"})
+    recaptcha = RecaptchaField()
     submit = SubmitField("Reset Password")
 
 
 @app.route('/')
 def index():
-
     images = Art.query.all()
-
     if not images:
         return render_template('index-for-anonymous.html', length=0)
-
     return_images = []
     art_data = []
-
     for image in images:
         base64_image = base64.b64encode(image.img)
         return_images.append(base64_image.decode("UTF-8"))
         art_data.append(image)
-
     if current_user.is_authenticated:
         return render_template('index-for-user.html', images=return_images, art_data=art_data, length=len(art_data), current_user=current_user)
-
     return render_template('index-for-anonymous.html', images=return_images, art_data=art_data, length=len(art_data))
 
 
 @ app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-
     msg = ""
-
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user:
@@ -217,9 +202,7 @@ def login():
         else:
             msg = "Username / Password is incorrect."
             return render_template('login.html', form=form, msg=msg)
-
     return render_template('login.html', form=form)
-
 
 @ app.route('/logout', methods=['GET', 'POST'])
 @ login_required
@@ -227,37 +210,35 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-
 @ app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-
-    # below function is for development purposes!
-    # generate_users()
-
+    msg=[]
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data,
+        if form.password.data == form.password_confirm.data and password_check(form.password.data):
+            hashed_password = bcrypt.generate_password_hash(form.password.data)
+            new_user = User(username=form.username.data,
                         password=hashed_password, balance=0,
                         email=form.email.data)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect((url_for('login')))
-
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect((url_for('login')))
+        else:
+            if (form.password.data != form.password_confirm.data):
+                msg.append("The password doesn't match!")
+            if not (password_check(form.password.data)):
+                msg.append("The password is not strong enough!")
+            return render_template('register.html', form=form, msg=msg)
     return render_template('register.html', form=form)
 
 
 @ app.route('/main')
 @ login_required
 def main():
-
     user = User.query.filter_by(username=current_user.username).first()
-    # task_list = Task.query.filter_by(ownerid=user.id)
-
     user_arts_num = len(user.owned_arts)
     images = user.owned_arts
     return_images = []
-
     for image in images:
         base64_image = base64.b64encode(image.img)
         return_images.append(base64_image.decode("UTF-8"))
@@ -265,53 +246,38 @@ def main():
     return render_template('main.html', user=user, images=return_images, length=user_arts_num)
 
 
-@ app.route('/buy/<int:art_id>')
+@ app.route('/buy/<int:art_id>', methods=['POST'])
 @ login_required
 def buy(art_id):
-
+    password = request.form.get('password')
+    if not bcrypt.check_password_hash(current_user.password, password):
+        return "wrong password!", 200
     art = Art.query.filter_by(id=art_id).first()
-
-    # if not enough balance
-    if current_user.balance < art.price:
-        return "Not enough balance, please top-up!", 200
-
-    # on every transaction, creator gets 5% commision
     commision = int(art.price * 0.05)
     net_amount = art.price - commision
-
     # current user loses money
     current_user.balance -= art.price
-
     # art owner gets 95% of the money which is the net amount
     art.owner[0].balance += net_amount
-
     # art creator gets 5% of the full price which is the comission
     art.creator[0].balance += commision
-
     new_transc = Transaction(
         art=art, seller_id=art.owner[0].id, buyer_id=current_user.id, price=art.price)
-
-    # art.transactions.append(new_transc)
-
     # transfer ownership to current user!
     art.owner[0] = current_user
-
     db.session.add(new_transc)
     db.session.commit()
-    return redirect(url_for('index'))
-
+    return redirect(url_for('main'))
 
 @ app.route('/edit-art/<int:art_id>', methods=['POST'])
 @ login_required
 def edit_art(art_id):
     picname = request.form.get('picname')
     price = request.form.get('price')
-
     art = Art.query.filter_by(id=art_id).first()
     art.name = picname
     art.price = price
     db.session.commit()
-
     return redirect(url_for('main'))
 
 
@@ -321,28 +287,19 @@ def upload():
     pic = request.files['pic']
     if not pic:
         return 'Not Picture!', 400
-
     picname = request.form.get('picname')
     price = request.form.get('price')
-
     filename = secure_filename(pic.filename)
     mimetype = pic.mimetype
     if not filename or not mimetype:
         return 'Bad upload!', 400
-
-    # art = Art(img=pic.read(), name=picname, mimetype=mimetype,
-    #           creator_id=current_user.id, owner_id=current_user.id, price=price)
-
     art = Art(img=pic.read(), name=picname, mimetype=mimetype, price=price)
     db.session.add(art)
     user = User.query.filter_by(id=current_user.id).first()
     user.owned_arts.append(art)
     user.created_arts.append(art)
-
     db.session.commit()
-
     return redirect((url_for('main')))
-
 
 @ app.route('/topup', methods=['POST'])
 @ login_required
@@ -353,7 +310,6 @@ def topup():
     db.session.commit()
     return redirect(url_for('main'))
 
-
 def generate_users():
     db.session.add(User(username='nicho', password=bcrypt.generate_password_hash(
         '123123'), balance=100))
@@ -363,14 +319,13 @@ def generate_users():
         '123123'), balance=100))
     db.session.commit()
 
-
 @ app.route('/art/<int:art_id>')
+@ login_required
 def transactions(art_id):
     art = Art.query.filter_by(id=art_id).first()
-
+    form = AuthenticateForm()
     base64_image = base64.b64encode(art.img)
     outer_array = []
-
     for transaction in art.transactions:
         inner_array = []
         user = User.query.filter_by(id=transaction.seller_id).first()
@@ -380,14 +335,13 @@ def transactions(art_id):
         inner_array.append(transaction.price)
         inner_array.append(transaction.date)
         outer_array.append(inner_array)
-
-    return render_template("art-details.html", image=base64_image.decode("UTF-8"), art_data=art, transaction_info=outer_array)
-
+    return render_template("art-details.html", image=base64_image.decode("UTF-8"), 
+    art_data=art, transaction_info=outer_array, current_user=current_user,
+    form=form)
 
 def send_email(app, msg):
     with app.app_context():
         mail.send(msg)
-
 
 def send_reset_email(user):
     token = user.get_reset_token()
@@ -401,10 +355,8 @@ If you did not make this request then simply ignore this email and no changes wi
 '''
     Thread(target=send_email, args=(app, msg)).start()
 
-
 @app.route('/reset_password', methods=['GET', 'POST'])
 def reset_request():
-    form = RequestResetForm()
     form = RequestResetForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -412,7 +364,24 @@ def reset_request():
         flash('An email has been sent with instructions to reset your password.', 'info')
         return redirect(url_for('login'))
     return render_template('reset_request.html', title='Reset Password', form=form)
-
+    
+def password_check(password):
+    """
+        8 characters length or more
+        1 digit or more
+        1 symbol or more
+        1 uppercase letter or more
+        1 lowercase letter or more
+    """
+    length_error = len(password) < 8
+    digit_error = re.search(r"\d", password) is None
+    uppercase_error = re.search(r"[A-Z]", password) is None
+    lowercase_error = re.search(r"[a-z]", password) is None
+    symbol_error = re.search(r"[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]', password) is None
+    password_ok = not ( length_error or digit_error or uppercase_error or lowercase_error or symbol_error )
+    if password_ok:
+        return True
+    return False
 
 @app.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_token(token):
@@ -421,18 +390,25 @@ def reset_token(token):
         flash('That is an invalid or expired token', 'warning')
         return redirect(url_for('reset_request'))
     form = ResetPasswordForm()
+    msg=[]
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(
+        if form.password.data == form.password_confirm.data and password_check(form.password.data):
+            hashed_password = bcrypt.generate_password_hash(
             form.password.data).decode('utf-8')
-        user.password = hashed_password
-        db.session.commit()
-        flash('Your password has been updated! You are now able to log in', 'success')
-        return redirect(url_for('login'))
+            user.password = hashed_password
+            db.session.commit()
+            flash('Your password has been updated! You are now able to log in', 'success')
+            return redirect(url_for('login'))
+        else:
+            if (form.password.data != form.password_confirm.data):
+                msg.append("The password doesn't match!")
+            
+            if not (password_check(form.password.data)):
+                msg.append("The password is not strong enough!")
+
+            return render_template('reset_token.html', title='Reset Password', form=form, msg=msg)
     return render_template('reset_token.html', title='Reset Password', form=form)
 
-
 if __name__ == '__main__':
-
     db.create_all()
-
     app.run(debug=True)
--- testing.py
+++
@@ -1,9 +0,0 @@
-from app import db, User, Art, Transaction
-
-art = Art.query.filter_by(id=1).first()
-print(art.transactions)
-for transaction in art.transactions:
-    print(transaction)
-    print(transaction.seller_id)
-    print(transaction.buyer_id)
-    print(transaction.art)
